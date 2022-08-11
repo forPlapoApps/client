5f8f16605a108f348b9c6cb563a1044032375aa0
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-11 17:16:44 +0900

    数字選択フォームの作成

--- pages/rooms/[uid].jsx
+++ pages/rooms/[uid].jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 
 export default function RoomsUid() {
+  const score_list = [1, 2, 3, 5, 8, 13, 21, 44]
   const [name, setName] = useState()
 
   const keyPress = (e) => {
@@ -13,6 +14,10 @@ export default function RoomsUid() {
   const deleteName = () => {
     localStorage.removeItem('userName')
     setName("")
+  }
+
+  const sendScore = (e) => {
+    console.log(e.target.value)
   }
 
   useEffect(() => {
@@ -32,10 +37,16 @@ export default function RoomsUid() {
   } else {
     return (
       <>
-        <p>Hello, {name}!! </p>
-        <button onClick={deleteName}>Change Name</button>
+        <div className="flex">
+          <p>Hello, {name}!! </p>
+          <button onClick={deleteName} className="w-20 border border-gray-500">Change Name</button>
+        </div>
 
-        
+        <select onChange={sendScore} name="score" className="w-20 border border-gray-500">
+          { score_list.map((score) => (
+            <option value={score} key={score}>{score}</option>
+          ))}
+        </select>
       </>
     )
   }
