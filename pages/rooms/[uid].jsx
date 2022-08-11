791ebb72daf17b333c28d347848e9b827d361616
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-11 17:04:20 +0900

    feat: ユーザー名入力フォームを設置

--- pages/rooms/[uid].jsx
+++ pages/rooms/[uid].jsx
@@ -1,9 +1,42 @@
+import { useState, useEffect } from "react"
+
 export default function RoomsUid() {
-  return (
-    <>
-      <h1>
-        create super future...
-      </h1>
-    </>
-  )
+  const [name, setName] = useState()
+
+  const keyPress = (e) => {
+    if(e.key == "Enter") {
+      localStorage.setItem('userName', e.target.value)
+      setName(e.target.value)
+    }
+  }
+
+  const deleteName = () => {
+    localStorage.removeItem('userName')
+    setName("")
+  }
+
+  useEffect(() => {
+    const inputName = localStorage.getItem('userName')
+    setName(inputName)
+  }, [name])
+
+  if(!name) {
+    return (
+      <>
+        <div className="flex">
+          <p>名前を入力してください</p>
+          <input type="text" className="border border-gray-500" onKeyPress={(e) => keyPress(e)} />
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <p>Hello, {name}!! </p>
+        <button onClick={deleteName}>Change Name</button>
+
+        
+      </>
+    )
+  }
 }
