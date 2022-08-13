60a1e9ec6c1e6e7ef7bc62688ceb537474686640
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-13 23:21:45 +0900

    style: ボタンの余計なクラスを削除

--- pages/components/FibonacciNumber.jsx
+++ pages/components/FibonacciNumber.jsx
@@ -8,7 +8,7 @@ export default function FibonacciNumber () {
       <p>選択されたのは：{ num }</p>
       <div className="flex gap-2">
         { [1, 2, 3, 5, 8, 13, 21, 42].map((num, i) => (
-          <label htmlFor={num} className="btn btn-active btn-primary" key={i} onClick={e => { setNum(e.target.value) }}>
+          <label htmlFor={num} className="btn btn-primary" key={i} onClick={e => { setNum(e.target.value) }}>
             {num} 
             <input type="radio" name="selectNumber" value={num} id={num} className="hidden" />
           </label>
