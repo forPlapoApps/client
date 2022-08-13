b74dd1029157bb07291d3a0aabe09c4c5c1a885c
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-13 23:35:30 +0900

    feat: fontawesomwの導入とCopyLinkコンポーネントの作成

---
+++ pages/components/CopyLink.jsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
+
+export default function CopyLink () {
+  return (
+    <>
+      <FontAwesomeIcon icon={faCopy} />
+    </>
+  )
+}
--- pages/rooms/[uid].jsx
+++ pages/rooms/[uid].jsx
@@ -1,9 +1,11 @@
+import CopyLink from "../components/CopyLink"
 import FibonacciNumber from "../components/FibonacciNumber"
 import OpenButton from "../components/OpenButton"
 
 export default function RoomsUid() {
   return (
     <>
+      <CopyLink />
       <OpenButton />
       <FibonacciNumber />
     </>
