b423e59018392f2d8b3dedc8b56417137a82819f
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-13 23:02:53 +0900

    feat: FibonacciNumber.jsxの実装

--- pages/components/FibonacciNumber.jsx
+++ pages/components/FibonacciNumber.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react"
+
 export default function FibonacciNumber () {
+  const [num, setNum] = useState(0)
+
   return (
     <>
-      
+      <p>選択されたのは：{ num }</p>
+      <div className="flex gap-2">
+        { [1, 2, 3, 5, 8, 13, 21, 42].map((num, i) => (
+          <label htmlFor={num} className="btn btn-active btn-primary" key={i} onClick={e => { setNum(e.target.value) }}>
+            {num} 
+            <input type="radio" name="selectNumber" value={num} id={num} className="hidden" />
+          </label>
+        )) }
+      </div>
     </>
   )
 }
