b05bad3daa919c528ba50161d0d010a81eba5600
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-30 12:08:54 +0900

    fix: createContextの初期値忘れを追加

--- pages/rooms/[uid].jsx
+++ pages/rooms/[uid].jsx
@@ -9,7 +9,7 @@ import { io } from "socket.io-client"
 
 const socket = io("http://localhost:5000")
 
-export const RoomsUidContext = createContext()
+export const RoomsUidContext = createContext({})
 
 export default function RoomsUid() {
   const [name, setName] = useState("")
