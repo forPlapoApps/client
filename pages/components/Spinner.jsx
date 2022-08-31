df5afd61af9846206c4b43ee7812689a55c05729
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-31 14:28:32 +0900

    style: spinnerのスタイル編集

--- pages/components/Spinner.jsx
+++ pages/components/Spinner.jsx
@@ -1,10 +1,12 @@
 export default function Spinner() {
   return (
     <>
-      <div className='flex justify-center'>
-        <div className='animate-ping h-2 w-2 bg-blue-600 rounded-full'></div>
-        <div className='animate-ping h-2 w-2 bg-blue-600 rounded-full mx-4'></div>
-        <div className='animate-ping h-2 w-2 bg-blue-600 rounded-full'></div>
+      <div className='w-full'>
+        <div className='flex mx-auto h-60 w-fit'>
+          <div className='animate-ping h-2 w-2 bg-blue-600 rounded-full m-auto'></div>
+          <div className='animate-ping h-2 w-2 bg-blue-600 rounded-full m-auto mx-4'></div>
+          <div className='animate-ping h-2 w-2 bg-blue-600 rounded-full m-auto'></div>
+        </div>
       </div>
     </>
   )
