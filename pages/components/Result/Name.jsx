c79273c5941b612df2d695835977da1f926dd734
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-31 22:00:00 +0900

    ResultNameにて、自分の名前だと色を変える

--- pages/components/Result/Name.jsx
+++ pages/components/Result/Name.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react"
+import { RoomsUidContext } from "../../rooms/[uid]"
+
 export default function ResultName(props) {
+  const { name } = useContext(RoomsUidContext) 
+  const isMyName = (value) => {
+    return value === name
+  }
+
   return (
     <>
       <div className='flex'>
-        <p className='mx-auto font-bold'>{props.name}</p>
+        <p className={`mx-auto font-bold ${isMyName(props.name) ? 'text-accent text-xl' : ''}`}>{props.name}</p>
       </div>
     </>
   )
--- pages/components/Title.jsx
+++ pages/components/Title.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 export default function Title() {
   return (
     <>
+      {/* 離れたときにコンポーネントが破棄されてないかも */}
       <Link href={'/'}>
         <p className='text-primary font-bold underline my-auto text-xl cursor-pointer'>forPlapoApps</p>
       </Link>
