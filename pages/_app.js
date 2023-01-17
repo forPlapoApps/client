a0156f1193a79bc7b6b8573917a00a622df73d68
hulk-higakijin <hm385.chejptks@gmail.com>
2023-01-17 18:42:38 +0900

    feat: metaタグの設定 title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>forPlapoApps | 工数見積もりの革命</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
