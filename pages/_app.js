54a6ce9e3dd57356aea784049ea19301482eb35c
hulk-higakijin <hm385.chejptks@gmail.com>
2023-01-17 19:15:39 +0900

    feat: メタタグの設定 画像

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>forPlapoApps | 工数見積もりの革命</title>
         <meta name="description" content="全てはミーティングのために。forPlapoAppsは工数見積もりに革命を起こします。" key="description" />
+        <meta property="og:image" content="../public/thumbnail.jpg" key="image" />
       </Head>
       <Component {...pageProps} />
     </>
