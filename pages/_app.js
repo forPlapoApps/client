8100416ae5f461e825ab466cad8013faf0a46971
hulk-higakijin <hm385.chejptks@gmail.com>
2023-01-17 19:19:17 +0900

    fix: 画像は絶対パスを指定するため、cloudinaryにアップロードした画像を使用

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>forPlapoApps | 工数見積もりの革命</title>
         <meta name="description" content="全てはミーティングのために。forPlapoAppsは工数見積もりに革命を起こします。" key="description" />
-        <meta property="og:image" content="../public/thumbnail.jpg" key="image" />
+        <meta property="og:image" content="https://res.cloudinary.com/dlzivlqnw/image/upload/v1673950688/forPlapoApps/fwsz2dcfifixnz7j69yv.jpg" key="image" />
       </Head>
       <Component {...pageProps} />
     </>
