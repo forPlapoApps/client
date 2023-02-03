fea9e4b9ba3ec28c779ef5190be48c7312d78ba4
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-03 12:06:45 +0900

    refactor: srcディレクトリを作成（blitzを参考にしたディレクトリ構成にするため）

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import '../styles/globals.css'
+import 'app/core/styles/index.css'
 import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
